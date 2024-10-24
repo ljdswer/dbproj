097628d4a8063e98d2ff19b68d9b64d10f7cd917
Alekseenkov Daniil <ljdswer@gmail.com>
2024-10-24 23:07:08 +0300

    Add logging of SQL requests

--- app/src/db/__init__.py
+++ app/src/db/__init__.py
@@ -1,4 +1,5 @@
 from mariadb import DataError
+from logging import getLogger
 from .sql_provider import SQLProvider
 from .context_manager import DBContextManager
 
@@ -7,7 +8,9 @@ def alter(dbconfig: dict, query: str, args: tuple) -> None:
         cur.execute(query, args)
 
 def select(dbconfig: dict, query: str, args: tuple) -> list:
+    logger = getLogger(__name__)
     with DBContextManager(dbconfig) as cur:
+        logger.debug(f"Executing {query} with: {args}")
         cur.execute(query, args)
         result = [i for i in cur]
     return result
