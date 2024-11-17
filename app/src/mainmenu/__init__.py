4fa313be5fe069e57ee771824df821b28bf29aaa
Daniil Alekseenkov <ljdswer@gmail.com>
2024-11-17 09:39:34 +0000

    Move main menu html from generic templates to its blueprint

--- app/src/mainmenu/__init__.py
+++ app/src/mainmenu/__init__.py
@@ -1,10 +1,14 @@
+from os import path
 from flask import (
     Blueprint,
     session,
     render_template,
 )
 
-mainmenu_blueprint = Blueprint("mainmenu_bp", __name__)
+module_path = path.dirname(path.abspath(__file__))
+mainmenu_blueprint = Blueprint(
+    "mainmenu_bp", __name__, template_folder=path.join(module_path, "web"))
+
 
 @mainmenu_blueprint.route("/")
 def index():
