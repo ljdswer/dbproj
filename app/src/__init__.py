b6cb757ce50933e413b794af6d5f910de2c919a2
Daniil Alekseenkov <ljdswer@gmail.com>
2024-11-29 19:30:46 +0000

    Make blueprint imports part of the app context

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -3,12 +3,6 @@ import logging
 import sys
 import tomllib
 from flask import Flask, render_template, session
-from .mainmenu import mainmenu_blueprint
-from .auth import auth_blueprint, auth_key_name
-from .requests import requests_blueprint
-from .external import external_blueprint
-from .clients import clients_blueprint
-from .reports import reports_blueprint
 
 
 def create_app():
@@ -22,15 +16,21 @@ def create_app():
     )
     load_config(app, "config")
     logger.info("Starting app...")
+
+    with app.app_context():
+        from .mainmenu import mainmenu_blueprint
+        from .auth import auth_blueprint, auth_key_name
+        from .requests import requests_blueprint
+        from .external import external_blueprint
+        from .clients import clients_blueprint
+        from .reports import reports_blueprint
     
-    app.register_blueprint(mainmenu_blueprint, url_prefix="/")
-    app.register_blueprint(auth_blueprint, url_prefix="/auth")
-    app.register_blueprint(requests_blueprint, url_prefix="/requests")
-    app.register_blueprint(external_blueprint, url_prefix="/external")
-    app.register_blueprint(clients_blueprint, url_prefix="/clients")
-    app.register_blueprint(reports_blueprint, url_prefix="/reports")
-
-
+        app.register_blueprint(mainmenu_blueprint, url_prefix="/")
+        app.register_blueprint(auth_blueprint, url_prefix="/auth")
+        app.register_blueprint(requests_blueprint, url_prefix="/requests")
+        app.register_blueprint(external_blueprint, url_prefix="/external")
+        app.register_blueprint(clients_blueprint, url_prefix="/clients")
+        app.register_blueprint(reports_blueprint, url_prefix="/reports")
     return app
 
 
